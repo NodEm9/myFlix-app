dc48c7e73c3c6b3af334305957ad4e0bc1bd78ba
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-09-09 20:39:25 +0200

    Create a cors option file and implement allowed URLs

--- config/corOptions.js
+++ config/corOptions.js
@@ -2,11 +2,11 @@ const allowedOrigins = require("./allowedOrign.js");
 
 
 const corsOptions = (origin, callback) => {
+  const msg = "The CORS policy for this site does not allow access from the specified Origin.";
   if (allowedOrigins.indexOf(origin) === -1 || !origin) {
-    const msg = "The CORS policy for this site does not allow access from the specified Origin.";
-    return callback(new Error(msg), false);
+    callback(null, true);
   }
-  return callback(null, true);
+  callback(new Error(msg), false);  
 };
 
 module.exports = corsOptions;
