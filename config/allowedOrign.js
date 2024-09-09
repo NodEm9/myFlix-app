2bc72691f666d2923fa47f70e69e54cf58b094f1
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-09-09 20:28:56 +0200

    Create a cors option file and implement allowed URLs

--- config/allowedOrign.js
+++ config/allowedOrign.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
   "http://localhost:4200/",
   "http://localhost:8080",
   "https://myflix-app-led6.onrender.com",
-  "https://myflix-movieoasis.netlify.app"
+  "https://myflix-movieoasis.netlify.app",
+  "https://nodem9.github.io/myFlix-angular-client/",
 ];
 
 module.exports = allowedOrigins;
--- config/corOptions.js
+++ config/corOptions.js
@@ -1,8 +1,8 @@
 const allowedOrigins = require("./allowedOrign.js");
 
+
 const corsOptions = (origin, callback) => {
-  if (!origin) return callback(null, true);
-  if (allowedOrigins.indexOf(origin) === -1) {
+  if (allowedOrigins.indexOf(origin) === -1 || !origin) {
     const msg = "The CORS policy for this site does not allow access from the specified Origin.";
     return callback(new Error(msg), false);
   }
